141a5dea1e0d95f005573433fa1c31be460b529c
hadesmile <hadesmile@gmail.com>
2016-04-14 20:32:13 +0800

    test code

---
+++ Five/src/main/java/logger/Statistics.java
@@ -0,0 +1,37 @@
+package logger;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import model.Cell;
+
+public class Statistics {
+//	private List<Node> nodes = new ArrayList<>();
+//	private Node current;
+//	private Map<Node, Node> map = new HashMap<>();
+//	public void addMove() {
+//		current = new Node(cell, value);
+//	}
+//	
+//	public void addNode(Cell parent,Cell child, int value) {
+//		Node node = new Node(child, value);
+//		if (parent == null) {
+//			current.children.add(node);
+//		} else {
+//			
+//		}
+//	}
+//	
+//	class Node {
+//		Cell move;
+//		int value;
+//		List<Node> children = new ArrayList<>();
+//		
+//		public Node(Cell cell, int value) {
+//			this.move = cell;
+//			this.value = value;
+//		}
+//	}
+}
